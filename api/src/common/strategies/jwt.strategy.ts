1f1595bd782aaddf8a3ca842bbb2f52387f54ef6
Cindy Marin Torres <cindymarintorres@hotmail.com>
2026-06-08 16:03:29 -0500

    fix: Agregar algortimo HS256 en validacion de JWT Token

--- api/src/common/strategies/jwt.strategy.ts
+++ api/src/common/strategies/jwt.strategy.ts
@@ -21,6 +21,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
       secretOrKey: jwtSecret,
+      algorithms: ['HS256'],
     });
   }
 
