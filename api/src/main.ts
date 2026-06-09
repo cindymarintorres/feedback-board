e6e16dc13690d61cb12aacaf6167878083d31abe
Cindy Marin Torres <cindymarintorres@hotmail.com>
2026-06-09 15:34:34 -0500

    chore: fix @types/express version conflict and update env config

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ZodValidationPipe } from 'nestjs-zod';
 import { LoggingInterceptor } from './common/interceptors/interceptor.logging';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(cookieParser());
+  
   const port = process.env.PORT || 3000;
   app.setGlobalPrefix('api');
 
