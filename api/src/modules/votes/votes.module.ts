6e255702916d4d9b27f412e61e5e28668754af33
Cindy Marin Torres <cindymarintorres@hotmail.com>
2026-07-04 23:24:37 -0500

    fix(votes): register mail queue in module

--- api/src/modules/votes/votes.module.ts
+++ api/src/modules/votes/votes.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common'
 import { VotesController } from './votes.controller'
 import { VotesService } from './votes.service'
+import { MailModule } from '../mail/mail.module'
+import { BullModule } from '@nestjs/bull'
 
 @Module({
+  imports: [
+    MailModule,                                    // Modulo de Mail
+    BullModule.registerQueue({ name: 'mail' })],    // Modulo Bull/Colas],
   controllers: [VotesController],
   providers: [VotesService],
 })
